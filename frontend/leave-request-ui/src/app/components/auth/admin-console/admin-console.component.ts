29acd8c819574390e8eaca3c5d6e3dafbf80aa9a
KevinJGomez <kevinjgomezofficial@gmail.com>
2024-07-25 16:24:18 +0530

    changes

--- frontend/leave-request-ui/src/app/components/auth/admin-console/admin-console.component.ts
+++ frontend/leave-request-ui/src/app/components/auth/admin-console/admin-console.component.ts
@@ -1,19 +1,35 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthServiceService } from '../../../services/auth-service.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { catchError, map, first } from 'rxjs/operators';
+
 @Component({
   selector: 'app-admin-console',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,ReactiveFormsModule],
   templateUrl: './admin-console.component.html',
   styleUrl: './admin-console.component.scss'
 })
-export class AdminConsoleComponent {
-  user = {
-    username: '',
-    password: '',
-    role: ''
-  };
+export class AdminConsoleComponent implements OnInit{
+  myForm!: FormGroup;
+  sendData: any = {};
+  constructor(private authService: AuthServiceService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.myForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required,Validators.minLength(6)]),
+      role: new FormControl('', Validators.required) 
+    });
+  }
 
   users = [
     { id: 1, username: 'john', role: 'ADMIN' },
@@ -21,15 +37,59 @@ export class AdminConsoleComponent {
   ];
 
   onSubmit() {
-    const newUser = {
-      id: this.users.length + 1,
-      ...this.user
-    };
-    this.users.push(newUser);
-    this.user = {
-      username: '',
-      password: '',
-      role: ''
-    };
+    if (this.myForm.status === 'INVALID') {
+      if(this.myForm.get('password')?.invalid){
+        Swal.fire({
+          icon: 'error',
+          title: 'Invalid Submission',
+          text: 'Password Must have atleast 6 characters!',
+        });
+      }
+      else{
+        Swal.fire({
+          icon: 'error',
+          title: 'Invalid Submission',
+          text: 'Provide All Required Information!',
+        });
+      }
+    } else {
+      this.sendData = {
+        username: this.myForm.controls['username'].value,
+        password: this.myForm.controls['password'].value,
+        role: this.myForm.controls['role'].value
+      };
+      this.authService
+        .register(this.sendData)
+        .pipe(
+          first(),
+          catchError((err) => {
+            return err;
+          })
+        )
+        .subscribe((data: any) => {
+          if(data.resCode !== "0"){
+            Swal.fire({
+              icon: 'error',
+              title: 'Creation Failed',
+              text: data.data,
+            });
+          }
+          else{
+            Swal.fire({
+              icon: 'success',
+              title: 'User Creation Successful!',
+              showConfirmButton: false,
+              timer: 1500
+            }).then(() => {
+              this.myForm.reset();
+            });
+          }
+          
+        });
+    }
+  }
+
+  resetForm(){
+    this.myForm.reset();
   }
 }
--- frontend/leave-request-ui/src/app/components/auth/login/login.component.ts
+++ frontend/leave-request-ui/src/app/components/auth/login/login.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit} from '@angular/core';
 import {
   FormGroup,
   FormControl,
-  Validators,
-  AsyncValidator,
+  Validators
 } from '@angular/forms';
 import { Router } from '@angular/router';
 import { catchError, map, first } from 'rxjs/operators';
